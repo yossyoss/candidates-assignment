b8e8b77649bd24ce3fb6fabd56628629360b5feb
yossi <yossi@hiredscore.com>
2021-07-20 18:19:39 +0300

    fixing ex5 and 5

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+#change ssh configuration
+echo "Host *" > /home/vagrant/.ssh/config && echo " StrictHostKeyChecking no" >> /home/vagrant/.ssh/config
+#copy server1 pub-key to server2 auth keys
+cat /vagrant/server1.key | tee -a /home/vagrant/.ssh/authorized_keys
+
+chown -R vagrant:vagrant /home/vagrant/
--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+read -p "full file path: " files
+read -p "destination server: " destination
+read -p "destination path: " path
+
+for file in $files;
+do
+  eval scp -3 "$file" vagrant@$destination:/$path/
+done
+
+
+echo "Total number of bytes copied ="
+eval du -bcl $files | awk 'END { print $1 }'
