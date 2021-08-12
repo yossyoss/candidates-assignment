e56caa295dba5235b28b23d0988d97111785f5b0
yossi <yossi@hiredscore.com>
2021-08-12 14:44:05 +0300

    Fixing ex6

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 #add fix to exercise6-fix here
-read -p "full file path: " files
-read -p "destination server: " destination
-read -p "destination path: " path
+if [[ $# -lt 2 ]]
+then
+	echo "please add more then 2 arguments"
+  exit 1
+fi
 
-for file in $files;
-do
-  eval scp -3 "$file" vagrant@$destination:/$path/
+ARRAY=( $@ )
+len=${#ARRAY[@]}
+DESTFOLDER=${ARRAY[$len-1]}
+SOURCES=${ARRAY[@]:0:$len-1}
+
+SRCHOST=`hostname`
+if [[ `hostname` == server1 ]] 
+then 
+	DESTHOST=server2
+else
+	DESTHOST=server1
+	#since this part doesnt exit in ex5 (we asked to allow ssh from server1 to server2 but not from 2 to 1 so we need to add the sme logic here)
+	if [[ -f .ssh/id_rsa && -f .ssh/id_rsa.pub ]]; then
+		ssh-keygen -N "" -f /home/vagrant/.ssh/id_rsa
+		cp /home/vagrant/.ssh/id_rsa.pub /vagrant/server2.key
+	fi
+fi
+
+scp -r $SOURCES vagrant@$DESTHOST:$DESTFOLDER 
+
+BYTESCOUNT=0
+for i in  $SOURCES ; do
+	let "`stat -c "%s"  $i` + $BCOUNT"
 done
-
-
-echo "Total number of bytes copied ="
-eval du -bcl $files | awk 'END { print $1 }'
+echo $BYTESCOUNT
